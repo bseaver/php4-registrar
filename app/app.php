d00cffb1cd3a2b6cbc4b30bc84a438e953301c1a
Benjamin T. Seaver and Stella Huayhuaca <benjamin@>
2017-02-28 13:58:26 -0800

    Add twig page on home route (app/app.php, views/)

--- app/app.php
+++ app/app.php
@@ -1,26 +1,29 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    // require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Student.php";
     // require_once __DIR__."/../src/Category.php";
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    $server = 'mysql:host=localhost:8889;dbname=to_do';
+    $server = 'mysql:host=localhost:8889;dbname=registrar';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     $app = new Silex\Application();
 
-    // $app->register(
-    //     new Silex\Provider\TwigServiceProvider(),
-    //     array('twig.path' => __DIR__.'/../views')
-    // );
+    $app->register(
+        new Silex\Provider\TwigServiceProvider(),
+        array('twig.path' => __DIR__.'/../views')
+    );
 
     $app->get("/", function() use ($app) {
-        return 'Hello Registrar';
+        return $app['twig']->render(
+            'students.html.twig',
+            array('students' => Student::getAll())
+        );
     });
 
     $app->post("/post/student", function() use ($app) {
